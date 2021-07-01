d332ebef73048c08ee5ba2d25866c356e1aadf32
Amirov <a.amirov@infobot.pro>
2021-07-01 07:30:09 +0300

    initial :scissors:

---
+++ src/infobot-sdk/actions.ts
@@ -0,0 +1,215 @@
+import { v1 as uuid } from 'uuid'
+
+export const ACTIONS = {
+  PONG: 'PONG',
+  AUTH: 'auth',
+  MAKE_CALL: 'make-call',
+  HANGUP: 'hang-up',
+  ANSWER: 'answer',
+  RING: 'ring',
+  STOP_DELIVERY: 'stop-delivery',
+  STOP_TRY: 'stop-try',
+  START_CALL: 'start-call',
+  FINISH_CALL: 'finish-call',
+  START_AUDIO_STREAM: 'start-audio-stream',
+  STOP_AUDIO_STREAM: 'stop-audio-stream',
+  FORWARAD_AUDIO_STREAM: 'forward-audio-stream',
+  SEND_SMS: 'send-sms',
+  CALL_FORWARD: 'call-forward',
+  START_BACKGROUND_SOUND: 'start-background-sound',
+  STOP_BACKGROUND_SOUND: 'stop-background-sound',
+  CACHE_TTS: 'cache-tts',
+  START_AUDIO_RECORD: 'start-audio-record',
+  STOP_AUDIO_RECORD: 'stop-audio-record',
+  TTS_ACTION: 'tts',
+  PLAYBACK_ACTION: 'playback',
+  PLAYBACK_FILE: 'playback-file',
+  STOP_PLAYBACK: 'stop-playback',
+  FILE_RESPONSE: 'file-response',
+  START_SPEECH_RECOGNITION: 'start-speech-recognition',
+  STOP_SPEECH_RECOGNITION: 'stop-speech-recognition',
+}
+
+export function auth(appID: string, key: string) {
+  return { action: ACTIONS.AUTH, appID, key }
+}
+
+export function makeCall(params: any = {}) {
+  return { action: ACTIONS.MAKE_CALL, ...params }
+}
+
+export function hangup(reason: any) {
+  return {
+    action: ACTIONS.HANGUP,
+    params: {
+      reason: reason,
+    },
+  }
+}
+
+export function answer() {
+  return { action: ACTIONS.ANSWER }
+}
+
+export function ring() {
+  return { action: ACTIONS.RING }
+}
+
+export function stopDelivery(params: any = {}) {
+  return { action: ACTIONS.STOP_DELIVERY, params }
+}
+
+export function stopTry() {
+  return { action: ACTIONS.STOP_TRY }
+}
+
+export function startCall(params: any = {}) {
+  return { action: ACTIONS.START_CALL, params }
+}
+
+export function finishCall(params: any = {}) {
+  return { action: ACTIONS.FINISH_CALL, params }
+}
+
+export function pong() {
+  return { action: ACTIONS.PONG }
+}
+
+export function startAudioStream() {
+  return { action: ACTIONS.START_AUDIO_STREAM }
+}
+
+export function stopAudioStream() {
+  return { action: ACTIONS.STOP_AUDIO_STREAM }
+}
+
+export function forwardAudioStream(host: string, port: number | string) {
+  return {
+    action: ACTIONS.FORWARAD_AUDIO_STREAM,
+    params: { host, port },
+  }
+}
+
+export function sendSms(to: string, text: string, digital: any, short: any, from: string, id?: string | number) {
+  return { action: ACTIONS.SEND_SMS, to, text, from, digital, short, id: id || uuid() }
+}
+
+export function callFoward(to: string, message: string, headers = null) {
+  return {
+    action: ACTIONS.CALL_FORWARD,
+    params: { to, message, headers },
+  }
+}
+
+export function startBackgroundSound(url: string, volume: any, repeat: any) {
+  return {
+    action: ACTIONS.START_BACKGROUND_SOUND,
+    params: {
+      url,
+      repeat,
+      volume,
+    },
+  }
+}
+
+export function cacheTTS(phrases: any) {
+  return {
+    action: ACTIONS.CACHE_TTS,
+    params: {
+      phrases: phrases,
+    },
+  }
+}
+
+export function stopBackgroundSound() {
+  return { action: ACTIONS.STOP_BACKGROUND_SOUND }
+}
+
+export function startAudioRecord(recordingID: string | number, format: any) {
+  return {
+    action: ACTIONS.START_AUDIO_RECORD,
+    params: {
+      format,
+      recordingID,
+    },
+  }
+}
+
+export function stopAudioRecord(recordingID: string | number) {
+  return {
+    action: ACTIONS.STOP_AUDIO_RECORD,
+    params: {
+      recordingID,
+    },
+  }
+}
+
+export function ttsAction(text: string, params: any = {}, ssml: any) {
+  return {
+    action: ACTIONS.TTS_ACTION,
+    params: Object.assign({
+      text,
+      ssml,
+      playbackID: this.id,
+      ...params,
+    }),
+  }
+}
+
+export function playbackAction(playbackID: string | number, url: string) {
+  return {
+    action: ACTIONS.PLAYBACK_ACTION,
+    params: { url, playbackID },
+  }
+}
+
+export function playbackFile(playbackID: string | number, fileHash: any, fileType: string) {
+  return {
+    action: ACTIONS.PLAYBACK_FILE,
+    params: { fileHash, fileType, playbackID },
+  }
+}
+
+export function fileResponse(playbackID: string | number, fileData: string, fileType: string) {
+  return {
+    action: ACTIONS.FILE_RESPONSE,
+    params: { fileData, fileType, playbackID },
+  }
+}
+
+export function stopPlayback(playbackID: string | number) {
+  return {
+    action: ACTIONS.STOP_PLAYBACK,
+    params: {
+      playbackID,
+    },
+  }
+}
+
+export function startSpeechRecognition(
+  sessionID: string | number,
+  provider: any,
+  language: any,
+  grammar: any,
+  timeout: any
+) {
+  return {
+    action: ACTIONS.START_SPEECH_RECOGNITION,
+    params: {
+      provider,
+      language,
+      grammar,
+      timeout,
+      sessionID,
+    },
+  }
+}
+
+export function stopSpeechRecognition(sessionID: string | number) {
+  return {
+    action: ACTIONS.STOP_SPEECH_RECOGNITION,
+    params: {
+      sessionID,
+    },
+  }
+}
---
+++ src/infobot-sdk/call.ts
@@ -0,0 +1,132 @@
+import { v4 as uuid } from 'uuid'
+import WebSocket from 'ws'
+import EventEmitter from 'events'
+import InfobotPlayback from './playback'
+import InfobotRecording from './recording'
+import InfobotRecognitionSession from './recognition'
+import * as actions from './actions'
+import { WS_EVENTS } from './events'
+
+export default class InfobotCall extends EventEmitter {
+  isConnected = false
+
+  constructor(public id: string = uuid(), private ws: WebSocket, public params: any = {}) {
+    super()
+    this.setMaxListeners(200)
+  }
+
+  processEvent(event: string, data: any, receiveData = null) {
+    this.emit(event, data)
+
+    if (receiveData) {
+      this.emit(WS_EVENTS.WS_RECEIVE, receiveData)
+    }
+  }
+
+  send(data: any) {
+    const dataWithCallID = { ...data, callID: this.id }
+    this.emit(WS_EVENTS.WS_SEND, dataWithCallID)
+    this.ws.send(JSON.stringify(dataWithCallID))
+  }
+
+  hangup(reason: any) {
+    this.send(actions.hangup(reason))
+  }
+
+  answer() {
+    this.isConnected = true
+    this.send(actions.answer())
+  }
+
+  ring() {
+    this.send(actions.ring())
+  }
+
+  stopDelivery() {
+    this.send(actions.stopDelivery())
+  }
+
+  stopTry() {
+    this.send(actions.stopTry())
+  }
+
+  start() {
+    this.send(actions.startCall())
+  }
+
+  finish() {
+    this.send(actions.finishCall())
+  }
+
+  pong() {
+    this.send(actions.pong())
+  }
+
+  startAudioStream() {
+    this.send(actions.startAudioStream())
+  }
+
+  stopAudioStream() {
+    this.send(actions.stopAudioStream())
+  }
+
+  forwardAudioStream(host: string, port: number | string) {
+    this.send(actions.forwardAudioStream(host, port))
+  }
+
+  sendSMS(to: string, text: string, digital: any, short: any, from: string) {
+    this.send(actions.sendSms(to, text, digital ? 1 : 0, short ? 1 : 0, from))
+  }
+
+  forward(to: string, message: string, headers = null) {
+    this.send(actions.callFoward(to, message, headers))
+  }
+
+  startBackgroundSound(url: string, volume: any, repeat: any) {
+    this.send(actions.startBackgroundSound(url, volume, repeat))
+  }
+
+  cacheTTS(phrases: any) {
+    this.send(actions.cacheTTS(phrases))
+  }
+
+  stopBackgroundSound() {
+    this.send(actions.stopBackgroundSound())
+  }
+
+  say(text: string, params: any, ssml: any): InfobotPlayback {
+    const playback = new InfobotPlayback(this)
+    playback.say(text, params, ssml)
+    return playback
+  }
+
+  playURL(url: string): InfobotPlayback {
+    const playback = new InfobotPlayback(this)
+    playback.playURL(url)
+    return playback
+  }
+
+  playFile(path: string): InfobotPlayback {
+    const playback = new InfobotPlayback(this)
+    playback.playFile(path)
+    return playback
+  }
+
+  startSpeechRecognition({ provider, language, grammar, timeout }): InfobotRecognitionSession {
+    const recognitionSession = new InfobotRecognitionSession(this)
+    recognitionSession.startSpeechRecognition(provider, language, grammar, timeout)
+    return recognitionSession
+  }
+
+  stopSpeechRecognition(): InfobotRecognitionSession {
+    const recognitionSession = new InfobotRecognitionSession(this)
+    recognitionSession.stopSpeechRecognition()
+    return recognitionSession
+  }
+
+  startAudioRecord(format: any): InfobotRecording {
+    const recording = new InfobotRecording(this)
+    recording.startRecording(format)
+    return recording
+  }
+}
